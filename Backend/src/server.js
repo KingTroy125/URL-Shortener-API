7f8c38fd9bfa9080a3e88f67ebbc93755c20d0ef
Tristan Hendricks <moonknight1355@gmail.com>
2026-05-23 16:00:39 +0200

    feat: connect to database in server initialization

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+// Connect to Database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
