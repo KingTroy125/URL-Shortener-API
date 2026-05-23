5c8c03aa44657b8d23d734dabf6de9fe860e087e
Tristan Hendricks <moonknight1355@gmail.com>
2026-05-23 13:25:57 +0200

    feat: add POST and GET endpoints for URL shortening and retrieval

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,6 +6,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// POST endpoint
+app.post("/api/shorten", (req, res) => {
+    res.status(200).send("URL shortened successfully!");
+});
+
+// GET endpoint
+app.get("/api/:shortCode", (req, res) => {
+    res.status(200).json({ 
+        message: "URL retrieved successfully!", 
+    });
+});
+
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on  http://localhost:${PORT}`);
